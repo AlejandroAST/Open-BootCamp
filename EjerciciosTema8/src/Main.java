16f6bda90d81f46e6667863e8937fa41b680d2ad
Alejandro <111458168+AlejandroAST@users.noreply.github.com>
2022-08-18 14:20:45 +0200

    Tema8 Ejercicio v1.1
    Acabado

--- EjerciciosTema8/src/Main.java
+++ EjerciciosTema8/src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("*********************************\n" +
+                "Tema 8 Ejercicio 1:\n" );
+        Persona persona=new Persona();
+        persona.setNombre("Alejandro");
+        persona.setEdad(30);
+        persona.setTelefono(985806094);
+        System.out.println("Persona:\n" +
+                "Nombre:"+persona.getNombre()+
+                "\nEdad:"+persona.getEdad()+
+                "\nTelefono:"+persona.getTelefono());
+    }
+
+}
+class Persona{
+    private int edad, telefono;
+    private String nombre;
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 }
