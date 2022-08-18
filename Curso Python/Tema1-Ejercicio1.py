40cadbfc16285289fc90aa36ca885ee63667e5fb
Alejandro <111458168+AlejandroAST@users.noreply.github.com>
2022-08-19 00:09:53 +0200

    Tema 1 Ejercicio 1 y 2

---
+++ Curso Python/Tema1-Ejercicio1.py
@@ -0,0 +1,8 @@
+print("<>----<> TEMA 1 <>----<>")
+print("+" + 13 * "-" + "+")
+print(("|" + " Ejercicio 1 "+ "|\n"), end="")
+print("+" + 13 * "-" + "+")
+
+print(" Hola, soy Alejandro")
+print(" Estoy empezando el curso de python")
+print(" Espero aprender mucho")
