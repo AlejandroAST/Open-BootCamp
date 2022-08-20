b900b99bae2f4aa0b179916a88261b1938bed428
Alejandro <111458168+AlejandroAST@users.noreply.github.com>
2022-08-20 17:46:33 +0200

    Tema 2 Ejercicio 1 v.1.2
    Estado : Resuelto

---
+++ Curso Python/Tema2/Tema2-Ejercicio1.py
@@ -0,0 +1,9 @@
+print("<>----<> TEMA 2 <>----<>")
+print("+" + 13 * "-" + "+")
+print(("|" + " Ejercicio 1 "+ "|\n"), end="")
+print("+" + 13 * "-" + "+")
+
+variable="Cadena de texto almacenada en la variable"
+tipo=type(variable)
+print("La variable contiene: "+variable+"\nSu tipo es:", end="")
+print(tipo)
