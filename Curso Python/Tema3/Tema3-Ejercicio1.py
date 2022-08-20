449ce573683f9ae1cc9c76d453d5a86cdf33161e
Alejandro <111458168+AlejandroAST@users.noreply.github.com>
2022-08-21 00:19:06 +0200

    Tema 3 Ejercicio 1 v1.1
    Estado Finalizado

---
+++ Curso Python/Tema3/Tema3-Ejercicio1.py
@@ -0,0 +1,19 @@
+print("<>----<> TEMA 3 <>----<>")
+print("+" + 13 * "-" + "+")
+print(("|" + " Ejercicio 1 "+ "|\n"), end="")
+print("+" + 13 * "-" + "+")
+
+nombre="Alejandro"
+apellidos="Fernández F"
+edad=30
+email="correo@gmail.com"
+casado=False
+conHijos=False
+listaAmigos=('Carlos', 'Gustavo', 'Lucia')
+peliculas={'cod01':"Regreso al Futuro", "cod02":"Indina Jones: La última cruzada"}
+
+print("Nombre: "+nombre+" | Apellidos: "+apellidos+" | Edad: "+str(edad)+
+      "\nEmail: "+email+" | Casado: "+ str(casado)+" | Con Hijos: "+str(conHijos)+
+      "\nLista Amigos: "+str(listaAmigos)+
+      "\nDiccionario de Peliculas: "+str(peliculas))
+
