e23acb7d2c62352206148b6572ddd4b1961b2211
Alejandro <111458168+AlejandroAST@users.noreply.github.com>
2022-08-20 17:51:06 +0200

    Tema 2 Ejercicio 2 v.1.2
    Estado: Acabado

---
+++ Curso Python/Tema2/Tema2-Ejercicio2.py
@@ -0,0 +1,12 @@
+print("<>----<> TEMA 2 <>----<>")
+print("+" + 13 * "-" + "+")
+print(("|" + " Ejercicio 2 "+ "|\n"), end="")
+print("+" + 13 * "-" + "+")
+
+variable="Cadena de texto almacenada en la variable"
+tipo=type(variable)
+print("La variable contiene: "+variable+"\nSu tipo es:", end="")
+print(tipo)
+print("Cambio el valor de la variable")
+variable="Variable Modificada v.2"
+print("La variable contiene: "+variable)
