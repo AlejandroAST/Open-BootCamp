3aa5be87f8807a6bb354b2e518d3aae437e86af3
Alejandro <111458168+AlejandroAST@users.noreply.github.com>
2022-08-22 12:59:46 +0200

    Tema 4 Ejercicio 3 v.1.1
    Estado Finalizado

--- Curso Python/Tema4/Tema4-Ejercicio3.py
+++ Curso Python/Tema4/Tema4-Ejercicio3.py
@@ -3,4 +3,10 @@ print("+" + 13 * "-" + "+")
 print(("|" + " Ejercicio 3 "+ "|\n"), end="")
 print("+" + 13 * "-" + "+")
 
+listaNumeros: int=[]    #Lista Vacia
 
+for i in range(1,101):
+    listaNumeros.append(101-i)
+
+print("Lista descendente desde 100 a 1\n-------------------------------")
+print(listaNumeros)
