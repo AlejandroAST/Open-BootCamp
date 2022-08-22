b5590000f24bbdc49abef7b2b1d6a0ef4daa1607
Alejandro <111458168+AlejandroAST@users.noreply.github.com>
2022-08-22 12:33:47 +0200

    Tema 4 Ejercicio 1 v.1.1
    Estado Finalizado

--- Curso Python/Tema4/Tema4-Ejercicio1.py
+++ Curso Python/Tema4/Tema4-Ejercicio1.py
@@ -3,4 +3,11 @@ print("+" + 13 * "-" + "+")
 print(("|" + " Ejercicio 1 "+ "|\n"), end="")
 print("+" + 13 * "-" + "+")
 
+edad=int(input("Escribe tu edad: "))
+
+if edad > 17:
+    print("Si eres mayor de edad con "+str(edad))
+else:
+    print("No eres mayor de edad con "+str(edad))
+
 
