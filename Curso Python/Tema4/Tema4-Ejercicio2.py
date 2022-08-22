67a5178c849315352a7d0aaa00a9b5799a3c266d
Alejandro <111458168+AlejandroAST@users.noreply.github.com>
2022-08-22 12:51:42 +0200

    Tema 4 Ejercicio 2 v.1.1
    Estado Finalizado

--- Curso Python/Tema4/Tema4-Ejercicio2.py
+++ Curso Python/Tema4/Tema4-Ejercicio2.py
@@ -4,3 +4,14 @@ print(("|" + " Ejercicio 2 "+ "|\n"), end="")
 print("+" + 13 * "-" + "+")
 
 
+numero_inicial: int=int(input("Especifica el numero inicial de la lista de impares: "))
+numero_final: int=int(input("Especifica el numero final de la lista de impares: "))
+listaImpares: int=[]#Lista vacia
+                      
+for valor in range(numero_inicial,numero_final+1):
+    if(valor%2) != 0:
+        listaImpares.append(valor)
+
+print("Lista IMPARES ---> ", end="")
+print(listaImpares)
+                      
