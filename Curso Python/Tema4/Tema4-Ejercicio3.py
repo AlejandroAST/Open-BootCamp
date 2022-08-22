da59e0ea579f70e3a2478d04dcf4476c6bec2247
Alejandro <111458168+AlejandroAST@users.noreply.github.com>
2022-08-22 13:08:25 +0200

    Tema 4 Ejercicio 3 v.1.2
    Cambiada la declaracion

--- Curso Python/Tema4/Tema4-Ejercicio3.py
+++ Curso Python/Tema4/Tema4-Ejercicio3.py
@@ -3,7 +3,7 @@ print("+" + 13 * "-" + "+")
 print(("|" + " Ejercicio 3 "+ "|\n"), end="")
 print("+" + 13 * "-" + "+")
 
-listaNumeros: int=[]    #Lista Vacia
+listaNumeros: [int]=[]    #Lista Vacia
 
 for i in range(1,101):
     listaNumeros.append(101-i)
