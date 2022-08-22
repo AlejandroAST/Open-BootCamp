f5dd8cb15edd639915cefc1bcd943130bdf5a63e
Alejandro <111458168+AlejandroAST@users.noreply.github.com>
2022-08-22 13:17:24 +0200

    Tema 4 Ejercicio 3 v.1.2
    Faltaba la comprobación de si el inicial era mayor que el final. Para evitar fuera de rango

--- Curso Python/Tema4/Tema4-Ejercicio2.py
+++ Curso Python/Tema4/Tema4-Ejercicio2.py
@@ -6,12 +6,20 @@ print("+" + 13 * "-" + "+")
 
 numero_inicial: int=int(input("Especifica el numero inicial de la lista de impares: "))
 numero_final: int=int(input("Especifica el numero final de la lista de impares: "))
-listaImpares: int=[]#Lista vacia
+listaImpares: [int]=[]#Lista vacia
+
+#Comprobar que el numero inicial no se mayor que el final
+while numero_inicial >= numero_final:
+    numero_inicial=int(input("Vuelve a escribir el numero inicial de la lista de impares: "))
+    numero_final=int(input("Vuelve a escribir  el numero final de la lista de impares: "))
+
                       
 for valor in range(numero_inicial,numero_final+1):
     if(valor%2) != 0:
         listaImpares.append(valor)
 
-print("Lista IMPARES ---> ", end="")
+print("Lista IMPARES desde "+str(numero_inicial)
+      +" hasta el "+str(numero_final)
+      +" ---> ", end="")
 print(listaImpares)
                       
