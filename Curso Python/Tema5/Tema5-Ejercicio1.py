c4ca10b2e71fd733249f71008dd185d63eb78545
Alejandro <111458168+AlejandroAST@users.noreply.github.com>
2022-08-23 10:44:00 +0200

    Tema 5 Ejercicio 1 v1.2
    Estado Resuelto

--- Curso Python/Tema5/Tema5-Ejercicio1.py
+++ Curso Python/Tema5/Tema5-Ejercicio1.py
@@ -4,5 +4,19 @@ print(("|" + " Ejercicio 1 "+ "|\n"), end="")
 print("+" + 13 * "-" + "+")
 
 
+def areaTriangulo(base, altura):
+    return base*altura/2
 
+def areaCirculo(radio):
+    return (radio**2)*3.14
 
+print("\nCalculo del area del triangulo"+
+      "\n*********************************")
+base: int=int(input("Base del Triangulo: "))
+altura: int=int(input("Altura del Triangulo: "))
+print("Area del triangulo: ", str(areaTriangulo(base, altura)))
+
+print("\nCalculo del area del Circulo"+
+      "\n*********************************")
+radio: float=float(input("Radio del Circulo: "))
+print("Area del circulo: ", str(areaCirculo(radio)))
