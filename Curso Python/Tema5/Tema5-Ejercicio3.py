02742659acffa7c75a30f20024510dd280e059db
Alejandro <111458168+AlejandroAST@users.noreply.github.com>
2022-08-23 11:16:31 +0200

    Tema 5 Ejercicio 3 v.1.2
    Estado Finalizado

--- Curso Python/Tema5/Tema5-Ejercicio3.py
+++ Curso Python/Tema5/Tema5-Ejercicio3.py
@@ -3,4 +3,17 @@ print("+" + 13 * "-" + "+")
 print(("|" + " Ejercicio 3 "+ "|\n"), end="")
 print("+" + 13 * "-" + "+")
 
+def añoBisiesto(año):
+  if año % 4 != 0:
+    return False
+  elif año % 100 != 0:
+    return True 
+  elif año % 400 != 0:
+    return False 
+  else:
+    return True
 
+año: int=int(input("Escribe el año que quieres comprobar: "))
+
+if añoBisiesto(año): print("El año ",str(año), " es bisiesto")
+else: print("El año ",str(año), " no es bisiesto")
