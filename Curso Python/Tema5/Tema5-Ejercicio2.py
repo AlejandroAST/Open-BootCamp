926514872f81644a86499c7578ce9d7e8aecf6c9
Alejandro <111458168+AlejandroAST@users.noreply.github.com>
2022-08-23 11:01:39 +0200

    Tema 5 Ejercicio 2 v1.2
    Estado Finalizada

--- Curso Python/Tema5/Tema5-Ejercicio2.py
+++ Curso Python/Tema5/Tema5-Ejercicio2.py
@@ -4,4 +4,14 @@ print(("|" + " Ejercicio 2 "+ "|\n"), end="")
 print("+" + 13 * "-" + "+")
 
 
+def esPrimo(numero):
+    for i in range(2, numero):
+        if numero % i ==0:
+            return False
+        
+    return True
 
+numero: int=int(input("Escribe el numero: "))
+
+if esPrimo(numero): print("El número ",str(numero), " es primo")
+else: print("El número ",str(numero), " no es primo")
