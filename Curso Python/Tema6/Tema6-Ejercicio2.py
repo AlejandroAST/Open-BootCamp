4b77ae51a7c5a82946b6bd1a07bed2df7b965198
Alejandro <111458168+AlejandroAST@users.noreply.github.com>
2022-08-24 16:57:32 +0200

    Tema6 Ejercicio2 v.1.2
    Estado Resuelto

--- Curso Python/Tema6/Tema6-Ejercicio2.py
+++ Curso Python/Tema6/Tema6-Ejercicio2.py
@@ -1,4 +1,36 @@
 print("<>----<> TEMA 6 <>----<>")
 print("+" + 13 * "-" + "+")
-print(("|" + " Ejercicio 2 "+ "|\n"), end="")
+print(("|" + " Ejercicio 2 " + "|\n"), end="")
 print("+" + 13 * "-" + "+")
+
+
+class Alumno:
+    nombre: str
+    nota: int
+
+    def __init__(self, nombre: str, nota: int):
+        self.nombre = nombre
+        self.nota = nota
+
+    def aprobado(self) -> bool:
+        """
+        Comprueba la nota
+        :return True Aprobado False Suspenso
+        """
+        if self.nota >= 5:
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        if self.aprobado():
+            return "Nombre alumno: {}, nota= {}, APROBADO".format(self.nombre, self.nota)
+        else:
+            return "Nombre alumno: {}, nota= {}, SUSPENSO".format(self.nombre, self.nota)
+
+
+alumno = Alumno("Alejandro", 7)
+alumno2 = Alumno("Diego", 4)
+
+print(alumno)
+print(alumno2)
