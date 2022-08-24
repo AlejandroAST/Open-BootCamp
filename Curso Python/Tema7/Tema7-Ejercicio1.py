60470e1b08fffd2b0e16d8827cc598c266623de8
Alejandro <111458168+AlejandroAST@users.noreply.github.com>
2022-08-24 20:10:56 +0200

    Tema 7 Ejercicio1 v.1.2
    Estado Finalizado

--- Curso Python/Tema7/Tema7-Ejercicio1.py
+++ Curso Python/Tema7/Tema7-Ejercicio1.py
@@ -1,4 +1,37 @@
-print("<>----<> TEMA 7 <>----<>")
-print("+" + 13 * "-" + "+")
-print(("|" + " Ejercicio 1 " + "|\n"), end="")
-print("+" + 13 * "-" + "+")
+import calculadora.operacionesBasicas as op
+def main():
+    """
+    Metodo Principal, muestra las opciones al usuario
+    """
+    print("<>----<> TEMA 7 <>----<>")
+    print("+" + 13 * "-" + "+")
+    print(("|" + " Ejercicio 1 " + "|\n"), end="")
+    print("+" + 13 * "-" + "+")
+
+    valor1: int = 0
+    valor2: int = 0
+    operacion: str = None
+
+    while "salir" != operacion:
+
+        valor1 = int(input("Escribe el primer valor: "))
+        valor2 = int(input("Escribe el segundo valor: "))
+        operacion = input("Que operacion básica quieres hacer(Suma, Resta, Multiplicacion o Division): ")
+
+        match operacion:
+            case "suma":
+                print(f"Suma de los {valor1} + {valor2} = {op.suma(valor1, valor2)}")
+            case "resta":
+                print(f"Resta de los {valor1} - {valor2} = {op.resta(valor1, valor2)}")
+            case "multiplicacion":
+                print(f"Multiplicación de los {valor1} * {valor2} = {op.multiplicacion(valor1, valor2)}")
+            case "division":
+                print(f"Division de los {valor1} / {valor2} = {op.division(valor1, valor2)}")
+
+
+        operacion = input("Para realizar otra operacion pulsa cualquier tecla y para salir escribe 'salir'").lower()
+
+    print("FIN DEL PROGRAMA")
+
+if __name__ == '__main__':
+    main()
---
+++ Curso Python/Tema7/calculadora/operacionesBasicas.py
@@ -0,0 +1,38 @@
+def suma(a: int, b: int) -> int:
+    """
+    Realiza la suma de los parametros recibidos
+    :param a: int
+    :param b: int
+    :return: int
+    """
+    return a + b
+
+
+def resta(a: int, b: int) -> int:
+    """
+    Realiza la resta de los parametros recibidos
+    :param a: int
+    :param b: int
+    :return: int
+    """
+    return a - b
+
+
+def multiplicacion(a: int, b: int) -> int:
+    """
+    Realiza la multiplicacion de los parametros recibidos
+    :param a: int
+    :param b: int
+    :return: int
+    """
+    return a * b
+
+
+def division(a: int, b: int) -> int:
+    """
+    Realiza la division de los parametros recibidos
+    :param a: int
+    :param b: int
+    :return: int
+    """
+    return a // b
