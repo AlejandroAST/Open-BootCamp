009a606105dff57f0b12cbb837613c6476b4a1ea
Alejandro <111458168+AlejandroAST@users.noreply.github.com>
2022-08-25 20:21:16 +0200

    Tema9-Ejercicio2.py.v.1.2 Finalizado

--- Curso Python/Tema9/Tema9-Ejercicio2.py
+++ Curso Python/Tema9/Tema9-Ejercicio2.py
@@ -1,3 +1,4 @@
+from functools import reduce
 
 def main():
     """
@@ -8,7 +9,31 @@ def main():
     print(("|" + " Ejercicio 2 " + "|\n"), end="")
     print("+" + 13 * "-" + "+")
 
-    impares=0
+    listaNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    print(f'Lista numeros inicial: {listaNumeros}')
+
+    listaImpares = list(filter(impares, listaNumeros))
+    # Version lambda
+    # listaImpares = list(filter((lambda a: a % 2 != 0), listaNumeros))
+
+    print(f'Lista numeros impares filtrada con filter: {listaImpares}')
+
+    resultado= reduce(suma, listaImpares)
+    # Version lambda
+    # resultado = reduce((lambda a, b: a+b), listaImpares)
+
+    print(f'Suma de los elementos con reduce: {resultado}')
+
+
+def impares(numero:int) -> bool:
+    if numero % 2 != 0:
+        return True
+
+    return False
+
+def suma(a: int, b: int) -> int:
+    return a+b
 
 if __name__ == '__main__':
     main()
