82e1f92fe5a0ef69d421f1da8d4c10c20ee1af52
Alejandro <111458168+AlejandroAST@users.noreply.github.com>
2022-08-25 09:19:21 +0200

    Tema8-Ejercicio2 v.1.1 Plantilla

---
+++ Curso Python/Tema8/Tema8-Ejercicio2.py
@@ -0,0 +1,12 @@
+def main():
+    """
+    Metodo Principal
+    """
+    print("<>----<> TEMA 8 <>----<>")
+    print("+" + 13 * "-" + "+")
+    print(("|" + " Ejercicio 2 " + "|\n"), end="")
+    print("+" + 13 * "-" + "+")
+
+
+if __name__ == '__main__':
+    main()
