775be61df0969a5f3ce4eb4b742377ee1c3ba1dd
Alejandro <111458168+AlejandroAST@users.noreply.github.com>
2022-08-25 19:48:57 +0200

    Tema9-Ejercicio1.py.v.1.1 Plantilla

---
+++ Curso Python/Tema9/Tema9-Ejercicio2.py
@@ -0,0 +1,14 @@
+
+def main():
+    """
+    Metodo Principal, muestra las opciones al usuario
+    """
+    print("<>----<> TEMA 9 <>----<>")
+    print("+" + 13 * "-" + "+")
+    print(("|" + " Ejercicio 2 " + "|\n"), end="")
+    print("+" + 13 * "-" + "+")
+
+
+
+if __name__ == '__main__':
+    main()
