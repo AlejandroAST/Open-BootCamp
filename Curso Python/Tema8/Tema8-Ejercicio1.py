78014ddc49b67981071a2283526fbdd25377f9a3
Alejandro <111458168+AlejandroAST@users.noreply.github.com>
2022-08-25 13:21:10 +0200

    Tema8-Ejercicio2 v.1.2 Finalizado

--- Curso Python/Tema8/Tema8-Ejercicio1.py
+++ Curso Python/Tema8/Tema8-Ejercicio1.py
@@ -1,3 +1,6 @@
+import pickle
+
+
 def main():
     """
     Metodo Principal
@@ -6,6 +9,29 @@ def main():
     print("+" + 13 * "-" + "+")
     print(("|" + " Ejercicio 1  " + "|\n"), end="")
     print("+" + 13 * "-" + "+")
+
+    datos: str = None
+    nombreFichero: str = 'Tema8-Ejercicio1-fichero.txt'
+
+    while datos == None:
+        datos = input("Que quieres escribir en el fichero: ")
+        escribirArchivo(nombreFichero, datos)
+
+    print("El archivo " + nombreFichero + " contiene: \n" + leerArchivo(nombreFichero))
+
+
+def escribirArchivo(nombreFichero: str, datos: str):
+    fichero = open('Tema8-Ejercicio1-fichero.txt', 'w')
+    fichero.write(datos)
+    fichero.close()
+
+
+def leerArchivo(nombreFichero: str) -> str:
+    datos: str = ''
+    fichero = open(nombreFichero, 'r')
+    datos = fichero.read()
+    fichero.close()
+    return datos
 
 
 if __name__ == '__main__':
