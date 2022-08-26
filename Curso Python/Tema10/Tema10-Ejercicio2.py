98ada81af07712e45c062f5731414d4846055e10
Alejandro <111458168+AlejandroAST@users.noreply.github.com>
2022-08-26 16:25:08 +0200

    Tema10-Ejercicio2.py.v.1.1 Plantilla

---
+++ Curso Python/Tema10/Tema10-Ejercicio2.py
@@ -0,0 +1,13 @@
+
+def main():
+    """
+    Metodo Principal
+    """
+    print("<>----<> TEMA 10 <>----<>")
+    print("+" + 13 * "-" + "+")
+    print(("|" + " Ejercicio 1 " + "|\n"), end="")
+    print("+" + 13 * "-" + "+")
+
+
+if __name__ == '__main__':
+    main()
