d5825dbf5130559001473e80b8e0fdfb1ebd651c
Alejandro <111458168+AlejandroAST@users.noreply.github.com>
2022-08-26 18:09:45 +0200

    Tema10-Ejercicio2.py.v.1.2 Finalizado

--- Curso Python/Tema10/Tema10-Ejercicio2.py
+++ Curso Python/Tema10/Tema10-Ejercicio2.py
@@ -1,13 +1,27 @@
 
-def main():
-    """
-    Metodo Principal
-    """
-    print("<>----<> TEMA 10 <>----<>")
-    print("+" + 13 * "-" + "+")
-    print(("|" + " Ejercicio 1 " + "|\n"), end="")
-    print("+" + 13 * "-" + "+")
+import tkinter
+from tkinter import ttk
+
+print("<>----<> TEMA 10 <>----<>")
+print("+" + 13 * "-" + "+")
+print(("|" + " Ejercicio 2 " + "|\n"), end="")
+print("+" + 13 * "-" + "+")
+
+window = tkinter.Tk()
+window.title("Tema 10 Ejercicio 2")
+window.geometry("350x150")
+
+window.columnconfigure(0, weight=1)
+window.columnconfigure(1, weight=4)
+
+elementos=('Rojo', 'Verde', 'Azul', 'Amarillo')
+elementosLista = tkinter.StringVar(value=elementos)
 
 
-if __name__ == '__main__':
-    main()
+titulo_opciones = ttk.Label(window, text="Elige una de las siguientes opciones:")
+titulo_opciones.grid(column=0, sticky=tkinter.W, row=1, pady=5, padx=5)
+
+lista = tkinter.Listbox(window, listvariable=elementosLista, height=6)
+lista.grid(column=0, sticky=tkinter.W, row=2, pady=5, padx=5)
+
+window.mainloop()
