72e4933f7aa5389d77ace4c06a2d9d202fd5daa6
Alejandro <111458168+AlejandroAST@users.noreply.github.com>
2022-08-27 12:07:37 +0200

    Tema11-Ejercicio1.py.v.1.1 Plantilla

---
+++ Curso Python/Tema11/Tema11-Ejercicio1.py
@@ -0,0 +1,12 @@
+
+def main():
+    """
+    Metodo Principal
+    """
+    print("<>----<> TEMA 11 <>----<>")
+    print("+" + 13 * "-" + "+")
+    print(("|" + " Ejercicio 1 " + "|\n"), end="")
+    print("+" + 13 * "-" + "+")
+
+if __name__ == '__main__':
+    main()
