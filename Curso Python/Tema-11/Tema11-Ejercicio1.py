ba9ce97083f2c9a7d391b631335e909898ad4e1a
Alejandro <111458168+AlejandroAST@users.noreply.github.com>
2022-08-27 21:01:18 +0200

    Tema11-Ejercicio1.py.v.1.4 Finalizada

--- Curso Python/Tema-11/Tema11-Ejercicio1.py
+++ Curso Python/Tema-11/Tema11-Ejercicio1.py
@@ -10,43 +10,57 @@ def main():
     print("+" + 13 * "-" + "+")
 
     print("Crear Tabla Alumnos")
-    #creaTablaAlumnos()
+    creaTablaAlumnos()
 
     print("Insertar Datos")
-    insertarAlumno(1, "Alejandro", "Fernandez")
+    listaId=[1,2,3,4,5,6,7,8]
+    listaNombres=["Alejandro", "Fernando","Diego","Marcos","Lucia","Dolores","Paloma","Sara"]
+    listaApellidos=["Fernandez", "Garcia","Garcia","Alvarez","Cuervo","Martinez","Franganillo","Tamargo"]
 
-    #alumno: str=input("Que alumno quieres buscar: ")
-    #mostrarAlumno(alumno)
-    print()
+    for i in range(0,8):
+        insertarAlumno(listaId[i], listaNombres[i], listaApellidos[i])
+        print(f'INSERT {i+1}')
 
-def mostrarAlumno(nombre: str):
-    pass
+    alumno: str=input("Que alumno quieres buscar: ")
+    alumnoBuscado=buscarAlumno(alumno)
+
+    if alumnoBuscado != None:
+        print(f'Datos Alumno Buscado \nID: {int(alumnoBuscado[0])} Nombre: {alumnoBuscado[1]} Apellidos: {alumnoBuscado[2]}')
+    else:
+        print("No se encuentra en la base de datos el alumno buscado "+alumno)
+
+
+def buscarAlumno(nombre: str):
+    conn = sqlite3.connect('BBDD_Alumnos.db')
+    cursor = conn.cursor()
+    query = '''select * from alumnos where nombre=(?);'''
+    row = cursor.execute(query, (nombre,))
+    alumnoBuscado=row.fetchone()
+    cursor.close()
+    conn.close()
+
+    return alumnoBuscado
 
 def insertarAlumno(id: int, nombre: str, apellido: str):
-    #El isolation_level es para permitir insertar datos deletes o updates, si quieres evitar el commit
     conn = sqlite3.connect('BBDD_Alumnos.db')
     cursor = conn.cursor()
 
-    query = '''INSERT INTO alumnos(id, nombre, apellido) VALUES(?,?,?)'''
-    print("Query: ", query)
-
-    row = cursor.execute(query, (id, nombre, apellido))
-
-    conn.commit()
-
-    cursor.close()
-    conn.close()
+    alumno=buscarAlumno(nombre)
+    if alumno == None:
+        query = '''INSERT INTO alumnos(id, nombre, apellido) VALUES(?,?,?)'''
+        row = cursor.execute(query, (id, nombre, apellido))
+        conn.commit()
+        cursor.close()
+        conn.close()
+    else:
+        cursor.close()
+        conn.close()
 
 def creaTablaAlumnos():
     conn = sqlite3.connect('BBDD_Alumnos.db')
     cursor = conn.cursor()
-
-    query = f'CREATE TABLE alumnos(id INT PRIMARY KEY, nombre TEXT, apellido TEXT);'
-
-    print("Query: ", query)
-
+    query = f'CREATE TABLE IF NOT EXISTS alumnos(id INT PRIMARY KEY, nombre TEXT, apellido TEXT);'
     row = cursor.execute(query)
-
     conn.commit()
     cursor.close()
     conn.close()
