d0f5fe99362fa8383039712f73433bd7adf04776
Alejandro <111458168+AlejandroAST@users.noreply.github.com>
2022-09-05 11:24:42 +0200

    Tema2-Ejercicio2.v.1.2 Finalizado

---
+++ Java Basico/Tema-2/src/impl/Ejercicio1.java
@@ -0,0 +1,47 @@
+package impl;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Ejercicio1 {
+
+
+
+    public static void main(String[] args) {
+        System.out.printf("" +
+                "<>---------<> TEMA 2 <>---------<>\n" +
+                "+-------------------------------+\n" +
+                "|          Ejercicio 1          |\n"+
+                "+-------------------------------+\n");
+
+        System.out.printf("Escribe el precio sin IVA ");
+
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        double precioSinIVA=0.0;
+        double precioConIVA=0.0, precioConIVAReducido10=0.0, precioConIVAReducido4=0.0;
+        try
+        {
+            System.out.printf("Valor sin IVA: ");
+            precioSinIVA = Double.parseDouble(br.readLine());
+
+            precioConIVA = precioSinIVA * 0.21 +precioSinIVA;
+            precioConIVAReducido10 = precioSinIVA * 0.10 +precioSinIVA;100
+            precioConIVAReducido4 = precioSinIVA * 0.04 +precioSinIVA;
+            System.out.println("----------------------------------------------------------------");
+            System.out.println ("El precio con IVA general: " +precioConIVA+"€");
+            System.out.println ("El precio con IVA reducido 10%: " +precioConIVAReducido10+"€");
+            System.out.println ("El precio con IVA reducido 4%: " +precioConIVAReducido4+"€");
+            System.out.println ("El precio sin IVA: " +precioSinIVA+"€");
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+
+}
