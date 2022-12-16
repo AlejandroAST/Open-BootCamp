3b865b3769f1ede5a2b4a0fc65e53f5d3edb8db6
Alejandro Fernández Fernández <111458168+AlejandroAST@users.noreply.github.com>
2022-12-16 19:23:31 +0100

    Ejercicio 1 v.1 finalizado

--- Java Avanzado/Tema-3-4-5/src/main/java/com/Tema345/controllers/BootcampersController.java
+++ Java Avanzado/Tema-3-4-5/src/main/java/com/Tema345/controllers/BootcampersController.java
@@ -1,9 +1,15 @@
 package com.Tema345.controllers;
 
 
+import com.Tema345.models.Bootcamper;
 import com.Tema345.services.BootcamperService;
-import jakarta.ws.rs.Path;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Response;
+import java.net.URI;
+import java.util.List;
 
 @Component
 @Path("/")
@@ -12,5 +18,76 @@ public class BootcampersController {
 
     public BootcampersController(BootcamperService bootcamperService){
         this.bootcamperService = bootcamperService;
+
+        this.bootcamperService.add(new Bootcamper("Juan", Math.random()));
+        this.bootcamperService.add(new Bootcamper("Marcos", Math.random()));
+        this.bootcamperService.add(new Bootcamper("Pedro", Math.random()));
+
+
     }
+
+    @GET
+    @Path("/")
+    public String portada() {
+        String pagina_inicial= "<!DOCTYPE html>\n" +
+                "<html lang=\"es\">\n" +
+                "<head>\n" +
+                "  <meta charset=\"utf-8\">\n" +
+                "  <title>HTML</title>\n" +
+                "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+                "  <link rel=\"stylesheet\" href=\"estilo.css\">\n" +
+                "</head>\n" +
+                "\n" +
+                "<body>\n" +
+                "  <p>Pagina Inicial del servicio REST </p>\n" +
+                "  <p>Tema 3 , 4 y 5  </p>\n" +
+                "  <p><a href=http://localhost:8080/bootcampers>Enlace a bootcampers </a> </p>\n" +
+                "</body>\n" +
+                "</html>\n" +
+                "\n";
+        return pagina_inicial;
+    }
+
+    @GET
+    @Path("/bootcampers")
+    @Produces("application/json")
+    public List<Bootcamper> listarTodos() {
+        return bootcamperService.getAll();
+    }
+
+    @GET
+    @Path("/bootcampers/{nombre}")
+    @Produces("application/json")
+    public Bootcamper listarUno(@PathParam("nombre") String nombre) {
+        return bootcamperService.get(nombre);
+    }
+
+
+    @POST
+    @Path("/bootcampers")
+    @Produces("application/json")
+    @Consumes("application/json")
+    public Response meterBootcamper(Bootcamper bootcamper) {
+        bootcamperService.add(bootcamper);
+
+        return Response.created(
+                URI.create("/bootcampers/" + bootcamper.getNombre())
+        ).build();
+    }
+
+    @PUT
+    @Path("/bootcampers")
+    @Produces("application/json")
+    @Consumes("application/json")
+    public Response modificarValorDeBootcamper(Bootcamper bootcamper) {
+        if (bootcamperService.modificar(bootcamper.getNombre(), bootcamper.getValor()) != null) {
+            return Response.ok(
+                    URI.create("/bootcampers/" + bootcamper.getNombre())
+            ).build();
+        }
+
+        return Response.notModified().build();
+
+    }
+
 }
--- Java Avanzado/Tema-3-4-5/src/main/java/com/Tema345/services/BootcamperService.java
+++ Java Avanzado/Tema-3-4-5/src/main/java/com/Tema345/services/BootcamperService.java
@@ -10,5 +10,34 @@ import java.util.List;
 @Service
 public class BootcamperService {
     private final List<Bootcamper> bootcampers= new ArrayList<Bootcamper>();
-    
+
+    public List<Bootcamper> getAll() {
+        return bootcampers;
+    }
+
+    public void add(Bootcamper bootcamper) {
+        bootcampers.add(bootcamper);
+    }
+
+    public Bootcamper get(String nombre) {
+        for (Bootcamper bootcamper : bootcampers) {
+            if (bootcamper.getNombre().equalsIgnoreCase(nombre)) {
+                return bootcamper;
+            }
+        }
+
+        return null;
+    }
+
+    public Bootcamper modificar(String nombre, double valor) {
+        for (Bootcamper bootcamper : bootcampers) {
+            if (bootcamper.getNombre().equalsIgnoreCase(nombre)) {
+                bootcamper.setValor(valor);
+                return bootcamper;
+            }
+        }
+
+        return null;
+    }
+
 }
